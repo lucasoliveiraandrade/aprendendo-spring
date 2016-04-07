951de5e90675e19c780f422a325b8219283ed74b
Lucas Andrade <lucas.andrade999@gmail.com>
2016-04-07 14:35:55 -0300

    melhoria home

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/HomeController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/HomeController.java
@@ -1,14 +1,22 @@
 package br.com.casadocodigo.loja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private ProdutoDAO produtoDAO;
+	
 	@RequestMapping("/")
-	public String index(){
-		System.out.println("passou por aqui");
-		return "home";
+	public ModelAndView index(){
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("produtos", produtoDAO.listar());
+		return modelAndView;
 	}
 }
